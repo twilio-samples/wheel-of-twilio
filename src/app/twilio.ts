b55b6b09d7255b4bed1f8820245864a35ffef354
Marius Obert <obatzer@gmail.com>
2024-04-03 15:52:06 +0200

    localize last whatsapp texts

--- src/app/twilio.ts
+++ src/app/twilio.ts
@@ -2,6 +2,12 @@
 
 import AccessToken, { SyncGrant } from "twilio/lib/jwt/AccessToken";
 
+import i18next from "i18next";
+import { getCountry } from "./api/incoming/helper";
+
+const en = require("../locale/en.json");
+const de = require("../locale/de.json");
+
 const {
   TWILIO_API_KEY = "",
   TWILIO_API_SECRET = "",
@@ -14,10 +20,27 @@ enum Privilege {
   FRONTEND = "FRONTEND",
 }
 
+async function localizeStringForPhoneNumber(
+  str: string,
+  phone: string,
+  name: string,
+  winningField?: string
+) {
+  await i18next.init({
+    lng: getCountry(phone)?.languages[0],
+    fallbackLng: "en",
+    resources: {
+      en,
+      de,
+    },
+  });
+
+  return i18next.t(str, { name, winningField });
+}
 
 export async function fetchToken() {
   const syncGrant = new SyncGrant({
-    serviceSid: SYNC_SERVICE_SID
+    serviceSid: SYNC_SERVICE_SID,
   });
 
   const token = new AccessToken(
@@ -39,11 +62,12 @@ export async function callWinners(winners: any[]) {
     accountSid: TWILIO_ACCOUNT_SID,
   });
 
-  winners.forEach((winner) => {
+  winners.forEach(async (winner) => {
+    const to = winner.sender.replace("whatsapp:", "");
     client.calls.create({
-      twiml: `<Response><Say>Congratulations ${winner.name}! You won!</Say></Response>`,
+      twiml: await localizeStringForPhoneNumber("winner", to, winner.name),
       from: NEXT_PUBLIC_TWILIO_PHONE_NUMBER,
-      to: winner.sender.replace("whatsapp:", ""),
+      to,
     });
   });
 }
@@ -53,9 +77,15 @@ export async function messageOthers(unluckyOnes: any[], winningNumber: string) {
     accountSid: TWILIO_ACCOUNT_SID,
   });
 
-  unluckyOnes.forEach((participant) => {
+  unluckyOnes.forEach(async (participant) => {
+    const body = await localizeStringForPhoneNumber(
+      "loser",
+      participant.sender,
+      participant.name,
+      winningNumber
+    );
     client.messages.create({
-      body: `Sorry ${participant.name}, you lost! The winning number was ${winningNumber}. \nBetter luck next time!`,
+      body,
       from: `whatsapp:${NEXT_PUBLIC_TWILIO_PHONE_NUMBER}`,
       to: participant.sender,
     });
