6f0c6f78f0c3f4ee105d4285d85e711af2cd8df5
Marius Obert <obatzer@gmail.com>
2024-06-03 14:55:45 +0200

    allow api invocations

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,5 +39,5 @@ function isAuthenticated(req: NextRequest) {
 
 // Step 3. Configure "Matching Paths" below to protect routes with HTTP Basic Auth
 export const config = {
-  matcher: "/:path*",
+  matcher: "/",
 };
