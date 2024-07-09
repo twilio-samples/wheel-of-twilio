f27a2868a07fc4e4b6a6d1788c7134df75a04def
Marius Obert <obatzer@gmail.com>
2024-07-09 18:23:41 +0200

    remove demo users

--- src/app/winners/page.tsx
+++ src/app/winners/page.tsx
@@ -10,16 +10,6 @@ export default async function WinnerPage({
 }) {
   let winners = await getWinners(searchParams.all === "true");
 
-  // generate 100 winners with random attributes
-  for (let i = 0; i < 100; i++) {
-    winners.push({
-      name: `Winner ${i}`,
-      sender: `whatsapp:${Math.floor(Math.random() * 100000000)}`,
-      stage:
-        Math.random() > 0.5 ? Stages.WINNER_CLAIMED : Stages.WINNER_UNCLAIMED,
-    });
-  }
-
   return (
     <div className="vh-full flex h-full  item justify-center relative">
       <DataTable columns={columns} data={winners} />
