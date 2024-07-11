849e8d0f93b94e102f070b4e5083443c4f7dd6ae
Marius Obert <obatzer@gmail.com>
2024-07-11 11:22:14 +0200

    add templates in more langauges too

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -92,14 +92,14 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
         username: TWILIO_API_KEY,
         password: TWILIO_API_SECRET,
       },
-    },
+    }
   );
   if (!data.mailer_sid) {
     console.error(`Verify service ${data.sid} does not have a mailer active`);
     process.exit(1);
   }
   console.log(
-    `Verify service ${data.sid} has been fetched and mailer is active ${data.mailer_sid}`,
+    `Verify service ${data.sid} has been fetched and mailer is active ${data.mailer_sid}`
   );
 })();
 
@@ -114,34 +114,34 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
   neededTemplates.forEach(async (template) => {
     let existingTemplate = existingTemplates.find(
       (existingTemplate) =>
-        existingTemplate.friendly_name === template.friendly_name,
+        existingTemplate.friendly_name === template.friendly_name
     );
     if (OVERRIDE_TEMPLATES) {
       if (existingTemplate) {
         try {
           await deleteWhatsAppTemplate(existingTemplate.sid);
           console.log(
-            `Template ${template.friendly_name} with SID ${existingTemplate.sid} has been deleted`,
+            `Template ${template.friendly_name} with SID ${existingTemplate.sid} has been deleted`
           );
-        } catch (e) {
+        } catch (e: any) {
           console.error(
             `Failed to delete template ${template.friendly_name} with SID ${existingTemplate.sid}`,
+            e?.message
           );
-          console.error(e);
           return;
         }
       }
     }
     if (existingTemplate && !OVERRIDE_TEMPLATES) {
       console.log(
-        `Template ${template.friendly_name} with SID ${existingTemplate.sid} already exists`,
+        `Template ${template.friendly_name} with SID ${existingTemplate.sid} already exists`
       );
     } else {
       try {
         existingTemplate = await createWhatsAppTemplate(template);
         const fileContent = fs.readFileSync(
           path.join(__dirname, `../locale/${template.language}.json`),
-          "utf8",
+          "utf8"
         );
 
         fs.writeFileSync(
@@ -155,8 +155,8 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
               },
             },
             null,
-            2,
-          ),
+            2
+          )
         );
       } catch (e) {
         console.error(`Failed to create template ${template.friendly_name}`);
@@ -164,7 +164,7 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
         return;
       }
       console.log(
-        `Template ${template.friendly_name} with SID ${existingTemplate.sid} has been created`,
+        `Template ${template.friendly_name} with SID ${existingTemplate.sid} has been created`
       );
     }
   });
@@ -184,7 +184,7 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
   }
 
   async function deleteWhatsAppTemplate(
-    sid: string,
+    sid: string
   ): Promise<WhatsAppTemplate> {
     const { data } = await axios.delete(
       `https://content.twilio.com/v1/Content/${sid}`,
@@ -196,13 +196,13 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
           username: TWILIO_API_KEY,
           password: TWILIO_API_SECRET,
         },
-      },
+      }
     );
     return data;
   }
 
   async function createWhatsAppTemplate(
-    template: WhatsAppTemplateConfig,
+    template: WhatsAppTemplateConfig
   ): Promise<WhatsAppTemplate> {
     const { data } = await axios.post(
       "https://content.twilio.com/v1/Content",
@@ -215,7 +215,7 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
           username: TWILIO_API_KEY,
           password: TWILIO_API_SECRET,
         },
-      },
+      }
     );
 
     return data;
