30b3860699ef5683bc8ef3c14001ced8ccba9743
Marius Obert <obatzer@gmail.com>
2024-07-12 11:46:38 +0200

    chore: Update clear script to clear bets and completed bets documents

--- src/scripts/clear.ts
+++ src/scripts/clear.ts
@@ -28,4 +28,30 @@ const client = twilio(TWILIO_API_KEY, TWILIO_API_SECRET, {
   );
 
   console.log(`All ${mapItems.length} attendees removed`);
+
+  const betsDoc = client.sync.v1
+    .services(SYNC_SERVICE_SID)
+    .documents("bets");
+
+  await betsDoc.update({
+    data: {
+      bets: {},
+      blocked: false,
+    },
+  });
+
+  console.log("Bets doc cleared");
+
+  const completedBetsDoc = client.sync.v1
+    .services(SYNC_SERVICE_SID)
+    .documents("completedBets");
+
+  await completedBetsDoc.update({
+    data: {
+      bets: {},
+    },
+  });
+
+  console.log("Completed bets doc cleared");
+
 })();
