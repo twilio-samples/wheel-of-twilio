9d695387be50acee7c78f90b7cc06a7ce8002a93
Marius Obert <obatzer@gmail.com>
2024-07-18 12:38:53 +0200

    add brb page

---
+++ src/app/brb/page.tsx
@@ -0,0 +1,13 @@
+
+export default async function WinnerPage({
+  searchParams,
+}: {
+  searchParams: { all: string };
+}) {
+
+  return (
+    <div className="vh-full flex h-full item justify-center align-center relative">
+      <h1 className="text-3xl my-auto text-white">The Wheel of Twilio will be right back after a break</h1>
+    </div>
+  );
+}
