336d39aa856e6386e12f0fa9e34ab3e428bc7ce4
Marius Obert <1873245+IObert@users.noreply.github.com>
2024-09-25 14:49:09 +0200

    Add feature flag to disable lead collection
    Add feature flag to control lead collection and update logic accordingly.
    
    * **sample.env**
      - Add `DISABLE_LEAD_COLLECTION` feature flag.
    
    * **src/app/api/incoming/helper.ts**
      - Check `DISABLE_LEAD_COLLECTION` flag before executing lead collection logic in `generateResponse` function.
      - If `DISABLE_LEAD_COLLECTION` is true, create user object with basic properties.
      - Send welcome message and then instructions to place a bet after 2 seconds.
    
    * **src/app/api/incoming/route.ts**
      - Pass `DISABLE_LEAD_COLLECTION` flag to `generateResponse` function.
    
    * **README.md**
      - Add instructions for the new `DISABLE_LEAD_COLLECTION` feature flag.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/twilio-samples/wheel-of-twilio?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/app/api/incoming/helper.ts
+++ src/app/api/incoming/helper.ts
@@ -1,5 +1,3 @@
-// Copyright 2024 Twilio Inc.
-
 import { ICountry, countries } from "countries-list";
 import { PhoneNumberUtil } from "google-libphonenumber";
 
@@ -25,6 +23,7 @@ const {
   NEXT_PUBLIC_WEDGES = "",
   OFFER_SMALL_PRIZES = "false",
   MAX_BETS_PER_USER = "0",
+  DISABLE_LEAD_COLLECTION = "false",
 } = process.env;
 
 const wedges = NEXT_PUBLIC_WEDGES.split(",");
@@ -76,97 +75,209 @@ export async function generateResponse(
       .update(currentUser?.sender || senderID || "")
       .digest("hex");
 
-    if (!currentUser) {
-      await attendeesMap.syncMapItems.create({
-        ttl: ONE_WEEK,
-        key: hashedSender,
-        data: {
-          name: senderName,
-          country,
-          sender: senderID,
-          submittedBets: 0,
-          stage: Stages.NEW_USER,
-        },
-      });
+    if (DISABLE_LEAD_COLLECTION === "false") {
+      if (!currentUser) {
+        await attendeesMap.syncMapItems.create({
+          ttl: ONE_WEEK,
+          key: hashedSender,
+          data: {
+            name: senderName,
+            country,
+            sender: senderID,
+            submittedBets: 0,
+            stage: Stages.NEW_USER,
+          },
+        });
 
-      twimlRes.message(i18next.t("welcome", { senderName }));
-    } else if (
-      currentUser.stage === Stages.NEW_USER ||
-      (currentUser.stage === Stages.VERIFYING && matchedEmail !== null)
-    ) {
-      if (matchedEmail === null) {
-        twimlRes.message(i18next.t("invalidEmail"));
-      } else {
-        try {
-          const verification = await client.verify.v2
-            .services(VERIFY_SERVICE_SID)
-            .verifications.create({
-              to: matchedEmail[0],
-              channel: "email",
-              // locale: lng,
-            });
-          await attendeesMap.syncMapItems(hashedSender).update({
-            data: {
-              ...currentUser,
-              email: matchedEmail[0],
-              stage: Stages.VERIFYING,
-              verificationSid: verification.sid,
-            },
-          });
-          twimlRes.message(i18next.t("sentEmail"));
-        } catch (e: any) {
-          if (e?.message?.startsWith("Invalid parameter `To`:")) {
-            twimlRes.message(i18next.t("invalidEmail"));
-          } else {
-            throw e;
-          }
-        }
-      }
-    } else if (currentUser.stage === Stages.VERIFYING) {
-      try {
-        const submittedCode = regexFor6ConsecutiveDigits.exec(messageContent);
-        if (submittedCode === null) {
-          throw new Error("Invalid code");
-        }
-        const verificationCheck = await client.verify.v2
-          .services(VERIFY_SERVICE_SID)
-          .verificationChecks.create({
-            verificationSid: currentUser.verificationSid,
-            code: submittedCode[0],
-          });
-
-        if (verificationCheck.status === "approved") {
-          await Promise.all([
-            attendeesMap.syncMapItems(hashedSender).update({
+        twimlRes.message(i18next.t("welcome", { senderName }));
+      } else if (
+        currentUser.stage === Stages.NEW_USER ||
+        (currentUser.stage === Stages.VERIFYING && matchedEmail !== null)
+      ) {
+        if (matchedEmail === null) {
+          twimlRes.message(i18next.t("invalidEmail"));
+        } else {
+          try {
+            const verification = await client.verify.v2
+              .services(VERIFY_SERVICE_SID)
+              .verifications.create({
+                to: matchedEmail[0],
+                channel: "email",
+                // locale: lng,
+              });
+            await attendeesMap.syncMapItems(hashedSender).update({
               data: {
                 ...currentUser,
-                stage: Stages.VERIFIED_USER,
+                email: matchedEmail[0],
+                stage: Stages.VERIFYING,
+                verificationSid: verification.sid,
               },
-            }),
-            client.messages.create({
-              contentSid: i18next.t("betTemplateSID"),
-              from: MESSAGING_SERVICE_SID,
-              to: currentUser.sender,
-            }),
-          ]);
+            });
+            twimlRes.message(i18next.t("sentEmail"));
+          } catch (e: any) {
+            if (e?.message?.startsWith("Invalid parameter `To`:")) {
+              twimlRes.message(i18next.t("invalidEmail"));
+            } else {
+              throw e;
+            }
+          }
         }
-      } catch (e: any) {
-        if (e.message !== "Invalid code") {
-          throw e;
+      } else if (currentUser.stage === Stages.VERIFYING) {
+        try {
+          const submittedCode = regexFor6ConsecutiveDigits.exec(messageContent);
+          if (submittedCode === null) {
+            throw new Error("Invalid code");
+          }
+          const verificationCheck = await client.verify.v2
+            .services(VERIFY_SERVICE_SID)
+            .verificationChecks.create({
+              verificationSid: currentUser.verificationSid,
+              code: submittedCode[0],
+            });
+
+          if (verificationCheck.status === "approved") {
+            await Promise.all([
+              attendeesMap.syncMapItems(hashedSender).update({
+                data: {
+                  ...currentUser,
+                  stage: Stages.VERIFIED_USER,
+                },
+              }),
+              client.messages.create({
+                contentSid: i18next.t("betTemplateSID"),
+                from: MESSAGING_SERVICE_SID,
+                to: currentUser.sender,
+              }),
+            ]);
+          }
+        } catch (e: any) {
+          if (e.message !== "Invalid code") {
+            throw e;
+          }
+          twimlRes.message(i18next.t("verificationFailed"));
         }
-        twimlRes.message(i18next.t("verificationFailed"));
+      } else if (currentUser.stage === Stages.VERIFIED_USER) {
+        if (betsDoc.data.blocked) {
+          twimlRes.message(i18next.t("betsClosed"));
+          // check if one of the wedges is a substring of the capitalized messageContent
+        } else if (
+          wedges.some((wedge) =>
+            capitalizeEachWord(messageContent).includes(capitalizeEachWord(wedge))
+          )
+        ) {
+          const bets = betsDoc.data.bets ? [...betsDoc.data.bets] : [];
+          //sort longest to shortest first
+          const selectedBet = wedges
+            .sort((a, b) => b.length - a.length)
+            .find((wedge) =>
+              capitalizeEachWord(messageContent).includes(
+                capitalizeEachWord(wedge)
+              )
+            );
+
+          const existingBet = bets.find((bet: any) => bet[0] === hashedSender);
+
+          const maxBetsReached =
+            parseInt(MAX_BETS_PER_USER) > 0 &&
+            currentUser.submittedBets >= parseInt(MAX_BETS_PER_USER);
+
+          if (!existingBet && maxBetsReached) {
+            twimlRes.message(i18next.t("maxBetsReached"));
+            return twimlRes.toString();
+          }
+
+          if (existingBet) {
+            existingBet[1] = selectedBet;
+          } else {
+            bets.push([hashedSender, selectedBet, senderName]);
+          }
+          await betsDoc.update({
+            data: {
+              ...betsDoc.data,
+              full: false,
+              bets: [...bets],
+            },
+          });
+          if (!existingBet) {
+            // inc counter only if new bet and if it was successfully added
+            await attendeesMap.syncMapItems(hashedSender).update({
+              data: {
+                ...currentUser,
+                submittedBets: currentUser.submittedBets + 1,
+                event: EVENT_NAME,
+              },
+            });
+          }
+
+          twimlRes.message(
+            i18next.t("betPlaced", {
+              senderName,
+              messageContent: selectedBet,
+            })
+          );
+        } else {
+          await client.messages.create({
+            contentSid: i18next.t("invalidBetTemplateSID"),
+            from: MESSAGING_SERVICE_SID,
+            to: currentUser.sender,
+          });
+        }
+      } else if (currentUser.stage === Stages.WINNER_UNCLAIMED) {
+        await client.messages.create({
+          body:
+            OFFER_SMALL_PRIZES === "true"
+              ? i18next.t("alreadyPlayedNotClaimedSmallPrize")
+              : i18next.t("alreadyPlayedNotClaimed"),
+          from: MESSAGING_SERVICE_SID,
+          to: currentUser.sender,
+        });
+      } else if (
+        currentUser.stage === Stages.WINNER_CLAIMED ||
+        currentUser.stage === Stages.RAFFLE_WINNER
+      ) {
+        await client.messages.create({
+          body: i18next.t("alreadyPlayedPrizeClaimed"),
+          from: MESSAGING_SERVICE_SID,
+          to: currentUser.sender,
+        });
+      } else {
+        await client.messages.create({
+          body: i18next.t("catchAllError"),
+          from: MESSAGING_SERVICE_SID,
+          to: currentUser.sender,
+        });
+        console.error("Unhandled stage", currentUser.stage, currentUser);
       }
-    } else if (currentUser.stage === Stages.VERIFIED_USER) {
-      if (betsDoc.data.blocked) {
+    } else {
+      if (!currentUser) {
+        await attendeesMap.syncMapItems.create({
+          ttl: ONE_WEEK,
+          key: hashedSender,
+          data: {
+            name: senderName,
+            sender: senderID,
+            submittedBets: 0,
+            stage: Stages.NEW_USER,
+          },
+        });
+
+        twimlRes.message(i18next.t("welcome", { senderName }));
+
+        setTimeout(async () => {
+          await client.messages.create({
+            contentSid: i18next.t("betTemplateSID"),
+            from: MESSAGING_SERVICE_SID,
+            to: senderID,
+          });
+        }, 2000);
+      } else if (betsDoc.data.blocked) {
         twimlRes.message(i18next.t("betsClosed"));
-        // check if one of the wedges is a substring of the capitalized messageContent
       } else if (
         wedges.some((wedge) =>
           capitalizeEachWord(messageContent).includes(capitalizeEachWord(wedge))
         )
       ) {
         const bets = betsDoc.data.bets ? [...betsDoc.data.bets] : [];
-        //sort longest to shortest first
         const selectedBet = wedges
           .sort((a, b) => b.length - a.length)
           .find((wedge) =>
@@ -179,7 +290,7 @@ export async function generateResponse(
 
         const maxBetsReached =
           parseInt(MAX_BETS_PER_USER) > 0 &&
-          currentUser.submittedBets >= parseInt(MAX_BETS_PER_USER);
+          currentUser?.submittedBets >= parseInt(MAX_BETS_PER_USER);
 
         if (!existingBet && maxBetsReached) {
           twimlRes.message(i18next.t("maxBetsReached"));
@@ -198,8 +309,7 @@ export async function generateResponse(
             bets: [...bets],
           },
         });
-        if (!existingBet) {
-          // inc counter only if new bet and if it was successfully added
+        if (!existingBet && currentUser) {
           await attendeesMap.syncMapItems(hashedSender).update({
             data: {
               ...currentUser,
@@ -219,39 +329,13 @@ export async function generateResponse(
         await client.messages.create({
           contentSid: i18next.t("invalidBetTemplateSID"),
           from: MESSAGING_SERVICE_SID,
-          to: currentUser.sender,
+          to: currentUser?.sender,
         });
       }
-    } else if (currentUser.stage === Stages.WINNER_UNCLAIMED) {
-      await client.messages.create({
-        body:
-          OFFER_SMALL_PRIZES === "true"
-            ? i18next.t("alreadyPlayedNotClaimedSmallPrize")
-            : i18next.t("alreadyPlayedNotClaimed"),
-        from: MESSAGING_SERVICE_SID,
-        to: currentUser.sender,
-      });
-    } else if (
-      currentUser.stage === Stages.WINNER_CLAIMED ||
-      currentUser.stage === Stages.RAFFLE_WINNER
-    ) {
-      await client.messages.create({
-        body: i18next.t("alreadyPlayedPrizeClaimed"),
-        from: MESSAGING_SERVICE_SID,
-        to: currentUser.sender,
-      });
-    } else {
-      await client.messages.create({
-        body: i18next.t("catchAllError"),
-        from: MESSAGING_SERVICE_SID,
-        to: currentUser.sender,
-      });
-      console.error("Unhandled stage", currentUser.stage, currentUser);
     }
   } catch (error: any) {
     if (error.code === 54006) {
       betsDoc.update({
-        // not sync on purpose, should return warning even if it cannot be updated
         data: {
           ...betsDoc.data,
           full: true,
