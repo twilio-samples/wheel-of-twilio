497e1ee814fe0865f04fee1d64399dc8d8621878
Marius Obert <obatzer@gmail.com>
2025-03-04 12:48:54 +0100

    All multiple breaks to /brb screen, fixes #22

--- src/app/brb/page.tsx
+++ src/app/brb/page.tsx
@@ -1,18 +1,42 @@
-export default async function WinnerPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ all: string }>;
+import { changeGameLock } from "../twilio";
+
+export default async function WinnerPage(props: {
+  searchParams: Promise<{ kind: "running" | "break" | "end" }>;
 }) {
+  const searchParams = await props.searchParams;
+  const kind = searchParams.kind;
+
+  let heading = "";
+  let subheading = "";
+
+  if (kind === "running") {
+    heading = "Game is running";
+    subheading = "The Wheel of Twilio is currently running";
+    changeGameLock(kind);
+  } else if (kind === "break") {
+    heading = "Be right back";
+    subheading = "The Wheel of Twilio will return after a break";
+    changeGameLock(kind);
+  } else if (kind === "end") {
+    heading = "Game has ended";
+    subheading = "The Wheel of Twilio has ended";
+    changeGameLock(kind);
+  } else {
+    heading = "Unknown state";
+    subheading = "Valid status are running, break, end";
+  }
+
   return (
     <div className="vh-full flex h-full item justify-center align-center relative">
-      <div className=" my-auto">
-        <h1 className="text-[#EF223A] text-8xl mb-6">
-          Be right back
-        </h1>
+      <img
+        src="/images/twilio.png"
+        alt="logo"
+        className="absolute w-1/3 mt-24"
+      />
+      <div className="my-auto text-center">
+        <h1 className="text-[#EF223A] text-8xl mb-6">{heading}</h1>
         {/* TODO change font here */}
-        <h1 className="text-3xl text-[#FDF7F4]">
-          The Wheel of Twilio will return after a break
-        </h1>
+        <h1 className="text-3xl text-[#FDF7F4]">{subheading}</h1>
       </div>
     </div>
   );
