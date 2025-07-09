63f26035d02c6c89eac916978d70931f2168de4d
Marius Obert <obatzer@gmail.com>
2025-07-09 15:34:28 +0200

    onsite layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,12 +99,12 @@ function App() {
 
   const ctaAndQrCode = (
     <>
-      <p className="ml-auto my-auto text-right col-span-2 font-extrabold text-2xl text-[#FDF7F4]">
+      <p className="ml-auto my-auto text-right col-span-2 font-extrabold text-4xl text-[#FDF7F4]">
         Scan the code and win prizes
       </p>
       <div>
         <QRCode
-          className="mx-auto w-30 h-30 p-1 bg-[#FDF7F4]"
+          className="mx-auto w-65 h-65 p-1 bg-[#FDF7F4]"
           value={`https://wa.me/${process.env.NEXT_PUBLIC_TWILIO_PHONE_NUMBER}?text=Hit%20send%20to%20start!`}
         />
         <p className="text-center text-lg text-gray-500">
@@ -118,10 +118,10 @@ function App() {
     <>
       {/* <img src="/images/twilio_devs.png" alt="logo" className="w-2/3 mb-10" />*/}
       <img src="/images/twilio.png" alt="logo" className="w-1/3 mb-16" />
-      <h1 className={`text-[#EF223A] text-5xl ${myFont.className} `}>
+      <h1 className={`text-[#EF223A] text-7xl ${myFont.className} `}>
         Builder is a mindset
       </h1>
-      <h1 className="text-[#FDF7F4] text-5xl font-medium mt-1">
+      <h1 className="text-[#FDF7F4] text-7xl font-medium mt-1">
         not a job title
       </h1>
     </>
@@ -171,22 +171,22 @@ function App() {
   });
 
   const verticalLayout = (
-    <div className="flex flex-col mt-16 w-full h-full ">
+    <div className="flex flex-col mt-12 w-full h-full ">
       <div className="flex flex-col text-3xl font-extrabold pb-8 w-4/5 mx-auto">
         {logoAndHeading}
       </div>
-      <div className="my-20">
+      <div className="my-16">
         {spinner}
-        <div className="w-4/5 mx-auto grid grid-cols-2 py-24  gap-7 space-around text-center text-xl font-semibold ">
+        <div className="w-4/5 mx-auto grid grid-cols-2 py-20 gap-6 space-around text-center text-xl font-semibold ">
           {fieldsWithBets}
         </div>
       </div>
 
-      <div className="absolute bottom-5  text-gray-500 text-xs ml-16 mb-2">
-        <div className="mx-auto grid grid-cols-3 gap-6 mb-5 ">
+      <div className="absolute bottom-5  text-gray-500 text-xs mb-2">
+        <div className="mx-auto grid grid-cols-3 gap-6 mb-5 mr-12 ">
           {ctaAndQrCode}
         </div>
-        {legalText}
+        <div className="mx-12">{legalText}</div>
       </div>
     </div>
   );
